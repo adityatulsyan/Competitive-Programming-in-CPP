1af0413ed9c56ae551c3800e46411c6429288cad
adityatulsyan <tulsyanaditya6@gmail.com>
2020-08-16 04:11:07 +0530

    Longest palindromic substring commented

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 #define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);
 
+// Given a string S, find the longest palindromic substring in S.
+
 string palindromic(string s){
 	int len = s.size();
 	bool table[len][len];
-
 	memset(table, false, sizeof(table));
-	int m = 1;
+
+	int m = 1; // m to store length of maximum length at the moment
 	string ans = s.substr(0, m);
 	
-	//substring of length 1 are palindromic -- base case
+	//substring of length 1 are palindromic -- base case for odd length substring
 	for(int i = 0; i <= len; i++){
 		table[i][i] = true;
 	}
@@ -28,7 +30,7 @@ string palindromic(string s){
 		for(int i = 0; i < len+1-k; i++){ //i is the starting index of the substring
 			int j = i+k-1; // j is the last index of the substring
 
-			if(k > 2 && table[i+1][j-1] && s[i] == s[j]){
+			if(table[i+1][j-1] && s[i] == s[j]){
 				table[i][j] = true;
 				if(k > m){
 					m = k;
@@ -41,6 +43,7 @@ string palindromic(string s){
 }
 
 int main(){
+	IOS;
 	string s;
 	cin >> s;
 	string ans = palindromic(s);
