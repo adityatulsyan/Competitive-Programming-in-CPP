4812c95e866bd09c91d7df1331d92cd1ba5febbc
adityatulsyan <tulsyanaditya6@gmail.com>
2020-09-09 14:15:15 +0530

    Changes in ques 2

--- graphs_GFG/12_min_cost_path.cpp
+++ graphs_GFG/12_min_cost_path.cpp
@@ -19,39 +19,40 @@ int main(){
     int count;
     cin >> count;
     while(count--){
-        int n, x = -1, y = -1;
+        int n, x, y;
         cin >> n;
         vector<vector<int>> vec(n, vector<int>(n));
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        vector<vector<int>> vis(n, vector<int>(n, INT_MAX));
         for(int i = 0; i < n; i++){
-            for(int j  =0; j < n; j++){
-                cin >> vec[i][j];
+            for(int j = 0; j < n; j++){
+                cin >> vec[i][j];   
             }
         }
         // cout << dfs(vec, vis, x, y) << endl;
 
-        queue<pair<int, int>> q, pq;
-        q.push({0, 0});
-        int ans, res = 0;
+        queue<pair<pair<int, int>, int>> q, pq;
+        q.push({{0, 0}, 0});
+        int val, res = 0, rem = 0;
 
         while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            ans = INT_MAX;
-            while(!q.empty()){
-                ans = min(ans, vec[x][y]);
-                if(x < n-1) pq.push({x+1, y});
-                if(y < n-1) pq.push({x, y+1});
-                if(x > 0) pq.push({x-1, y});
-                if(y > 0) pq.push({x, y-1});
-            }
-            while(!pq.empty()){
-                q.push(pq.front());
-                pq.pop();
-            }
-            res = res+ans;
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            val = q.front().second;
+            q.pop();
+            if (val + vec[x][y] < vis[x][y])
+                vis[x][y] = val + vec[x][y];
+            else continue;
+            if(x < n-1) q.push({{x+1, y}, vis[x][y]});
+            if(y < n-1) q.push({{x, y+1}, vis[x][y]});
+            if(x > 0) q.push({{x-1, y}, vis[x][y]});
+            if(y > 0) q.push({{x, y-1}, vis[x][y]});
         }
-        cout << res << endl;
+
+        // for(auto u: vis){
+        //     for(auto it: u) cout << it << " ";
+        //     cout << endl;
+        // }
+        cout << "\n" << vis[n-1][n-1] << endl;
     }
     return 0;
 }
